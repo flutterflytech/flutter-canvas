7b8f79c677f7be3a0cb4cc6ccba5127e7ee0d77f
Alok Kumar <alokiimtgn@gmail.com>
2021-03-02 12:25:22 +0530

    Code for AnimatedPadding was added

--- lib/animatedcanvas/impliciteanimations/ImpliciteAnimation.dart
+++ lib/animatedcanvas/impliciteanimations/ImpliciteAnimation.dart
@@ -22,80 +22,58 @@ class ImpliciteAnimationUI extends StatefulWidget {
 class _ImpliciteAnimationUIState extends State<ImpliciteAnimationUI> {
   Curve _curveName;
   bool selected = false;
+  double padValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.red,
-          height: MediaQuery.of(context).size.height * 1,
-          width: MediaQuery.of(context).size.width * 1,
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: RaisedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (selected) {
-                            selected = false;
-                          } else {
-                            selected = true;
-                          }
-                          _curveName = Curves.easeIn;
-                          // print("Curve Name $_curveName $selected");
-                        });
-                      },
-                      child: Text("Linear"),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: RaisedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (selected) {
-                            selected = false;
-                          } else {
-                            selected = true;
-                          }
-                          _curveName = Curves.fastOutSlowIn;
-                          // print("Curve Name $_curveName $selected");
-                        });
-                      },
-                      child: Text("Easy In Quint"),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: RaisedButton(
-                      onPressed: () {},
-                      child: Text("Linear"),
-                    ),
-                  ),
-                ],
-              ),
-              AnimatedContainer(
-                  width: selected ? 200.0 : 100.0,
-                  height: selected ? 100.0 : 200.0,
-                  color: selected ? Colors.white : Colors.blue,
-                  alignment: selected
-                      ? Alignment.center
-                      : AlignmentDirectional.topCenter,
-                  duration: Duration(seconds: 2),
-                  curve: Curves.decelerate,
-                  onEnd: () {
-                    print(_curveName);
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text("Animated Padding"),
+      ),
+      body: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height * 1,
+        width: MediaQuery.of(context).size.width * 1,
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (padValue != 150.0) {
+                        padValue = 150.0;
+                      } else {
+                        padValue = 0;
+                      }
+                      print(padValue);
+                    });
                   },
-                  child: Icon(Icons.account_balance)),
-            ],
-          ),
+                  child: Text(
+                    "Animate",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            AnimatedPadding(
+              padding: EdgeInsets.all(padValue),
+              duration: Duration(seconds: 2),
+              curve: Curves.linear,
+              child: Container(
+                height: 50,
+                width: 250,
+                color: Colors.orange,
+              ),
+            )
+          ],
         ),
       ),
-    );
+    ));
   }
 }
